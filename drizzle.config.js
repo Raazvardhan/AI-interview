36698f9dd947bda9eca1b3d634b6ea3902c86a11
Raj vardhan <139915269+Raazvardhan@users.noreply.github.com>
2024-07-30 03:45:07 +0530

    Delete drizzle.config.js

--- drizzle.config.js
+++
@@ -1,8 +0,0 @@
-/** @type { import("drizzle-kit").Config } */
-export default {
-    schema: "./utils/schema.js",
-    dialect: 'postgresql',
-    dbCredentials: {
-      url: process.env.NEXT_PUBLIC_DRIZZLE_DB_URL,
-    }
-  };
