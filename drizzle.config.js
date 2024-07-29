5a24bc285ed5b63bf4a499d869abfd3e3d5d941e
Raj vardhan <139915269+Raazvardhan@users.noreply.github.com>
2024-07-30 03:44:08 +0530

    Update drizzle.config.js

--- drizzle.config.js
+++ drizzle.config.js
@@ -3,6 +3,6 @@ export default {
     schema: "./utils/schema.js",
     dialect: 'postgresql',
     dbCredentials: {
-      url: 'postgresql://mocker_owner:9nmoX5edMAOW@ep-summer-recipe-a59dbr9e.us-east-2.aws.neon.tech/mocker?sslmode=require',
+      url: process.env.NEXT_PUBLIC_DRIZZLE_DB_URL,
     }
   };
