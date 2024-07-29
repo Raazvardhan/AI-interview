352386bd3a73ee1c2c407ec1e26246c6ad0a9a03
Raj vardhan <139915269+Raazvardhan@users.noreply.github.com>
2024-07-30 03:46:01 +0530

    Create drizzle.config.js

---
+++ drizzle.config.js
@@ -0,0 +1,8 @@
+/** @type { import("drizzle-kit").Config } */
+export default {
+    schema: "./utils/schema.js",
+    dialect: 'postgresql',
+    dbCredentials: {
+      url: process.env.NEXT_PUBLIC_DRIZZLE_DB_URL,
+    }
+  };
