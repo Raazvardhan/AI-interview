20e21eb3c2cfeace9964d0d21fc0aa3d89772b9c
Raj vardhan <139915269+Raazvardhan@users.noreply.github.com>
2024-08-03 00:59:48 +0530

    logo

--- app/layout.js
+++ app/layout.js
@@ -2,6 +2,7 @@ import { Outfit } from "next/font/google";
 import "./globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
 import { Toaster } from "@/components/ui/sonner";
+import Head from "next/head";
 
 
 const inter = Outfit({ subsets: ["latin"] });
@@ -9,6 +10,7 @@ const inter = Outfit({ subsets: ["latin"] });
 export const metadata = {
   title: "AI Interview Coach",
   description: "AI-powered interview",
+  icon: "/public/icon.png",
 };
 
 export default function RootLayout({ children }) {
